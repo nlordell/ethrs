828926cfdfeb28cf2d15425f411e3a4d8c6bb762
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-05-29 17:37:40 +0200

    added todo

---
+++ num/src/iter.rs
@@ -0,0 +1,29 @@
+//! Module contains iterator specific trait implementations.
+
+use crate::u256;
+use std::iter::{Product, Sum};
+use std::ops::{Add, Mul};
+
+impl Sum for u256 {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(u256::ZERO, Add::add)
+    }
+}
+
+impl Product for u256 {
+    fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(u256::ONE, Mul::mul)
+    }
+}
+
+impl<'a> Sum<&'a u256> for u256 {
+    fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(u256::ZERO, Add::add)
+    }
+}
+
+impl<'a> Product<&'a u256> for u256 {
+    fn product<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(u256::ONE, Mul::mul)
+    }
+}
--- num/src/lib.rs
+++ num/src/lib.rs
@@ -2,6 +2,7 @@ mod arith;
 mod convert;
 mod endian;
 pub mod intrinsics;
+mod iter;
 mod ops;
 
 pub use self::convert::AsU256;
@@ -30,3 +31,137 @@ impl u256 {
         u256::from_words(0, value)
     }
 }
+
+// TODO(nlordell):
+//
+// # Methods
+//
+// - checked_next_power_of_two
+//
+// - count_ones
+// - count_zeros
+//
+// - from_be
+// - from_be_bytes
+// - from_le
+// - from_le_bytes
+// - from_ne_bytes
+//
+// - from_str_radix
+//
+// - is_power_of_two
+//
+// - leading_ones
+// - leading_zeros
+//
+// - next_power_of_two
+//
+// - reverse_bits
+//
+// - rotate_left
+// - rotate_right
+//
+// - swap_bytes
+//
+// - to_be
+// - to_be_bytes
+// - to_le
+// - to_le_bytes
+// - to_ne_bytes
+//
+// - trailing_ones
+// - trailing_zeros
+//
+// - wrapping_next_power_of_two
+//
+// # Trait Implementations
+//
+// - AddAssign<&'_ u128>
+// - AddAssign<u128>
+//
+// - Binary
+//
+// - BitAndAssign<&'_ u128>
+// - BitAndAssign<u128>
+// - BitOrAssign<&'_ u128>
+// - BitOrAssign<u128>
+// - BitXorAssign<&'_ u128>
+// - BitXorAssign<u128>
+//
+// - Display
+//
+// - DivAssign<&'_ u128>
+// - DivAssign<u128>
+//
+// - FromStr
+//
+// - LowerExp
+// - LowerHex
+//
+// - MulAssign<&'_ u128>
+// - MulAssign<u128>
+//
+// - Octal
+//
+// - Ord
+// - PartialOrd<u128>
+//
+// - RemAssign<&'_ u128>
+// - RemAssign<u128>
+//
+// - ShlAssign<&'_ i128>
+// - ShlAssign<&'_ i16>
+// - ShlAssign<&'_ i32>
+// - ShlAssign<&'_ i64>
+// - ShlAssign<&'_ i8>
+// - ShlAssign<&'_ isize>
+// - ShlAssign<&'_ u128>
+// - ShlAssign<&'_ u16>
+// - ShlAssign<&'_ u32>
+// - ShlAssign<&'_ u64>
+// - ShlAssign<&'_ u8>
+// - ShlAssign<&'_ usize>
+// - ShlAssign<i128>
+// - ShlAssign<i16>
+// - ShlAssign<i32>
+// - ShlAssign<i64>
+// - ShlAssign<i8>
+// - ShlAssign<isize>
+// - ShlAssign<u128>
+// - ShlAssign<u16>
+// - ShlAssign<u32>
+// - ShlAssign<u64>
+// - ShlAssign<u8>
+// - ShlAssign<usize>
+// - ShrAssign<&'_ i128>
+// - ShrAssign<&'_ i16>
+// - ShrAssign<&'_ i32>
+// - ShrAssign<&'_ i64>
+// - ShrAssign<&'_ i8>
+// - ShrAssign<&'_ isize>
+// - ShrAssign<&'_ u128>
+// - ShrAssign<&'_ u16>
+// - ShrAssign<&'_ u32>
+// - ShrAssign<&'_ u64>
+// - ShrAssign<&'_ u8>
+// - ShrAssign<&'_ usize>
+// - ShrAssign<i128>
+// - ShrAssign<i16>
+// - ShrAssign<i32>
+// - ShrAssign<i64>
+// - ShrAssign<i8>
+// - ShrAssign<isize>
+// - ShrAssign<u128>
+// - ShrAssign<u16>
+// - ShrAssign<u32>
+// - ShrAssign<u64>
+// - ShrAssign<u8>
+// - ShrAssign<usize>
+//
+// - Step
+//
+// - SubAssign<&'_ u128>
+// - SubAssign<u128>
+//
+// - UpperExp
+// - UpperHex
